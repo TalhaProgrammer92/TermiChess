329e9dee545cf50c6b034706d74ce848d0ed276b
TalhaProgrammer92 <talha.code92@gmail.com>
2025-04-14 20:04:49 +0500

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     wcout << UI::generateAnsiCode(
-        "Hello, Rayan!",
+        "Hello, Rayan!",        //! Write your name here (You can change UI elements)
         UI::FGColor::DARK_GREEN,
         UI::BGColor::LIGHT_CYAN,
         UI::Style::ITALIC) << endl;
