0a09c3a3b2c402afd76d2cb91d52811730e36df2
TalhaProgrammer92 <talha.code92@gmail.com>
2025-04-15 21:36:56 +0500

    Fix

--- src/ui/ansi.cpp
+++ src/ui/ansi.cpp
@@ -3,5 +3,5 @@
 
 std::string UI::getANSICode(const std::string& str, const std::string& fgColor, const std::string& bgColor, const std::string& style)
 {
-    return fgColor + bgColor + style + str + RESET;
+    return fgColor + bgColor + style + str + UI::RESET;
 }
